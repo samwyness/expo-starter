3bb02fa3b31961ea5cbb5fa2a067ffbee05dbb4f
Sam Wyness <samwyness22@gmail.com>
2024-03-13 17:18:53 +1000

    chore(deps): install and configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  // plugins: ['react', 'react-native', 'react-hooks'],
+  // overrides: [
+  //   {
+  //     files: ['*.ts', '*.tsx'],
+  //     rules: {
+  //       'import/order': [
+  //         'warn',
+  //         {
+  //           'newlines-between': 'never',
+  //         },
+  //       ],
+
+  //       '@typescript-eslint/no-shadow': ['error'],
+
+  //       'no-shadow': 'off',
+  //       'no-undef': 'off',
+
+  //       'react/react-in-jsx-scope': 'off',
+  //       'react/jsx-boolean-value': ['warn', 'never'],
+  //       'react/jsx-closing-bracket-location': [
+  //         'warn',
+  //         { nonEmpty: 'after-props', selfClosing: 'tag-aligned' },
+  //       ],
+  //       'react/jsx-curly-brace-presence': ['warn', 'never'],
+  //       'react/jsx-curly-spacing': ['warn', { when: 'never' }],
+  //       'react/jsx-equals-spacing': ['warn', 'never'],
+  //       'react/jsx-first-prop-new-line': ['warn', 'multiline'],
+  //       'react/jsx-fragments': ['warn', 'syntax'],
+  //       'react/jsx-indent': ['warn', 2],
+  //       'react/jsx-indent-props': ['warn', 2],
+  //       'react/jsx-no-bind': ['warn', { allowArrowFunctions: true, allowFunctions: true }],
+  //       'react/jsx-no-duplicate-props': 'error',
+  //       'react/jsx-no-undef': 'error',
+  //       'react/jsx-props-no-multi-spaces': 'warn',
+  //       'react/jsx-tag-spacing': 'warn',
+  //       'react/jsx-uses-react': 'warn',
+  //       'react/jsx-uses-vars': 'warn',
+  //       'react/jsx-wrap-multilines': [
+  //         'warn',
+  //         { declaration: false, assignment: false, return: true, arrow: true },
+  //       ],
+  //       'react/no-access-state-in-setstate': 'warn',
+  //       'react/no-did-mount-set-state': 'warn',
+  //       'react/no-did-update-set-state': 'warn',
+  //       'react/no-direct-mutation-state': 'warn',
+  //       'react/no-redundant-should-component-update': 'warn',
+  //       'react/no-this-in-sfc': 'warn',
+  //       'react/no-unknown-property': 'warn',
+  //       'react/no-will-update-set-state': 'warn',
+  //       'react/require-render-return': 'warn',
+  //       'react/self-closing-comp': 'warn',
+
+  //       'react-hooks/rules-of-hooks': 'error',
+  //       'react-hooks/exhaustive-deps': 'warn',
+
+  //       'react-native/no-unused-styles': 'warn',
+  //       'react-native/no-inline-styles': 'warn',
+  //       'react-native/no-color-literals': 'warn',
+  //       'react-native/no-raw-text': 'off', // TODO: set to 'warn' once i18n is implemented
+  //       'react-native/no-single-element-style-arrays': 'warn',
+  //     },
+  //   },
+  // ],
+};
