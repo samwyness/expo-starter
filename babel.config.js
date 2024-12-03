11b0a822f85c74ce03b30399507ab068d78a6e7e
samwyness <samwyness22@gmail.com>
2024-12-03 18:26:03 +1000

    chore: remove unnecessary react compiler babel settings

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          'react-compiler': {
-            // Passed directly to the React Compiler Babel plugin.
-            compilationMode: 'strict',
-            panicThreshold: 'all_errors',
-          },
-          web: {
-            'react-compiler': {
-              // Web-only settings...
-            },
-          },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
