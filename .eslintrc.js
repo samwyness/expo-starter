ce5991514d0285876b6b0a5171c177aefc7238dc
Sam Wyness <samwyness22@gmail.com>
2024-12-03 18:43:10 +1000

    refactor: feature driven development structure (#10)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
+/**
+ * Create a restricted import zone for a specific feature.
+ *
+ * e.g. src/features/foo should not import from src/features/bar, etc.
+ *
+ * @param {string} featureName
+ */
+function createFeatureZone(featureName) {
+  return {
+    target: `./src/features/${featureName}`,
+    from: `./src/features`,
+    except: [`./${featureName}`],
+  };
+}
+
 module.exports = {
   root: true,
   extends: ['expo', 'prettier'],
@@ -26,11 +41,11 @@ module.exports = {
           // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
           ['^@?\\w'],
 
-          [
-            // Absolute imports and other imports such as `#/foo`.
-            // Anything not matched in another group.
-            '^',
+          // Absolute imports and other imports such as `#/foo`.
+          // Anything not matched in another group.
+          ['^'],
 
+          [
             // Relative imports starting with `../`
             '^\\.\\.(?!/?$)',
             '^\\.\\./?$',
@@ -43,6 +58,28 @@ module.exports = {
         ],
       },
     ],
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          // Disable cross-feature imports:
+          createFeatureZone('home'),
+          createFeatureZone('explore'),
+
+          // Enforce unidirectional codebase:
+          // e.g. src/app can import from src/features but not the other way around, etc.
+          {
+            target: './src/features',
+            from: './src/app',
+          },
+          {
+            target: ['./src/shared', './src/assets'],
+            from: ['./src/features', './src/app'],
+          },
+        ],
+      },
+    ],
+    'import/no-cycle': 'error',
 
     'react/react-in-jsx-scope': 'off',
 
